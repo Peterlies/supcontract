be0340c20f550c4f5e4247fd005a523ea8b74574
henry <hukegong1024@gmail.com>
2022-11-04 14:21:36 +0800

    update

---
+++ contracts/FDao/FlameFdtExchange.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract FlameFdtExchange is Ownable{
+    
+    constructor() {
+
+    }
+
+    function Exchange() public {
+
+    }
+}
