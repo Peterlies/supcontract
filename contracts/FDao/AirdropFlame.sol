fdb9e080cb9952f7ea0ef8b887c444f315bff033
henry <hukegong1024@gmail.com>
2022-11-09 11:55:09 +0800

    update

--- contracts/FDao/AirdropFlame.sol
+++ contracts/FDao/AirdropFlame.sol
@@ -3,11 +3,29 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract airdropFlame is Ownable{
+    address public flame;
+    address public controlStatus;
+    bool public Status;
+constructor () {    
+}   
+    function setflameAddress(address _flame) public onlyOwner{
+        flame = _flame;
+    }
+    function setControlStatusAddress(address controlAddress) public onlyOwner {
+        controlStatus =controlAddress;
+    }
+    function setStatus() external {
+        require(msg.sender == controlStatus, "is not control contract");
+        Status =!Status;
+    }
 
-constructor () {
-    
-}
+    function receiveAirdrop(uint _amount) public {
+        require(IERC20(flame).balanceOf(address(this)) > _amount, "amount is not enough");
+        IERC20(flame).transfer(msg.sender,_amount);
+    }
+
 
 }
