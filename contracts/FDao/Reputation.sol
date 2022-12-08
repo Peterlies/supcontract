9ccec12cc05206577d1ae2394b550855b23c8f47
henry-maker-commits <hukegong1024@gmail.com>
2022-12-08 15:40:53 +0800

    update

--- contracts/FDao/Reputation.sol
+++ contracts/FDao/Reputation.sol
@@ -12,18 +12,36 @@ interface ISoulAccount{
 contract Reputation is Ownable
 { 
     // mapping(address => uint256) public Reputation;
+    bool initStatus;
     uint256[] public coefficient;
     address[] public sbt;
     address public fireSoul;
-    constructor()  {}
+    constructor()  {
+    }
 
+    function initializer() public {
+        require(!initStatus, "inited");
+        sbt.push(address(0x43387c942d7dd16aEa3134c9c9Dc7687C41005B4));
+        sbt.push(address(0xb3CDC058F8910D95dADC1456F898E8a8458C053d));
+        coefficient.push(1);
+        coefficient.push(2);
+        // sbt[1] = address(0xb3CDC058F8910D95dADC1456F898E8a8458C053d);
+        initStatus = true;
+    }
     //onlyOwner
-    function setSBTAddress(address[] memory _sbt) public onlyOwner {
-        sbt = _sbt;
+    function addSBTAddress(address _sbt, uint256 _coefficient) public onlyOwner {
+        sbt.push(_sbt);
+        coefficient.push(_coefficient);
     }
-    function setCoefficient(uint256[] memory _coefficient) public onlyOwner{
-        coefficient = _coefficient;
+    function setSBTAddress(uint256 num, address _sbt) public onlyOwner {
+        require(num < sbt.length, "num is bigger than length");
+        sbt[num] = _sbt;
     }
+    function setCoefficient(uint256 num, uint256 _coefficient) public onlyOwner {
+        require(num < coefficient.length, "num is bigger than length");
+        coefficient[num] = _coefficient;
+    }
+
     function setFireSoulAddress(address _fireSoul) public onlyOwner {
         fireSoul = _fireSoul;
     }
@@ -31,7 +49,9 @@ contract Reputation is Ownable
     function checkReputation(address _user) public view returns(uint256) {
         uint256 ReputationPoint;
         for(uint i = 0 ; i < sbt.length; i ++) {
-            ReputationPoint =  IERC20(sbt[i]).balanceOf(ISoulAccount(fireSoul).getSoulAccount(_user))*coefficient[i] +ReputationPoint ; 
+            // ReputationPoint =  IERC20(sbt[i]).balanceOf(ISoulAccount(fireSoul).getSoulAccount(_user))*coefficient[i] +ReputationPoint ; 
+            ReputationPoint =  IERC20(sbt[i]).balanceOf(_user)*coefficient[i] +ReputationPoint ; 
+
         }
         return ReputationPoint;
     }
