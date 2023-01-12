3d3a9a2ac9f4a07ce11d51057eda772c41a7bab3
henry-maker-commits <hukegong1024@gmail.com>
2023-01-12 14:36:24 +0800

    update

--- contracts/FDao/interface/ITreasuryDistributionContract.sol
+++ contracts/FDao/interface/ITreasuryDistributionContract.sol
@@ -1,7 +1,7 @@
 //	SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-interface IMinistryOfFinance {
+interface ITreasuryDistributionContract {
   function AllocationFund() external;
   function setSourceOfIncome(uint num,uint tokenNum,uint256 amount) external;
 }
