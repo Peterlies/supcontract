b222d498dcaa91a64e4fdf65a2869a24aa926210
henry-maker-commits <hukegong1024@gmail.com>
2023-02-08 13:57:56 +0800

    ii

--- contracts/FDao/TreasuryDistributionContract.sol
+++ contracts/FDao/TreasuryDistributionContract.sol
@@ -27,8 +27,6 @@ contract TreasuryDistributionContract is Initializable,UUPSUpgradeable,AccessCon
     mapping(uint =>mapping(uint => uint256[])) public sourceOfIncome;
     mapping(uint => address) public tokenList;
     mapping(address => bool) public allowAddr;
-    //0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3 pancake
-    //0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D uniswap
     constructor() initializer {
     }
     function initialize() public initializer {
