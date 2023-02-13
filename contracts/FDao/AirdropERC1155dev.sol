a9761d251505327615d1db6a2325c0bd31f65d0a
henry-maker-commits <hukegong1024@gmail.com>
2023-02-13 10:43:45 +0800

    aa

--- contracts/FDao/AirdropERC1155dev.sol
+++ contracts/FDao/AirdropERC1155dev.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "./interface/IFirePassport.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+import "./interface/IFirePassport.sol";
+
 
 contract AirdropERC1155 is ERC1155Holder{
     IERC1155 public token;
