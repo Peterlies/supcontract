6ebfc505318c7e4e8191c2c231d574997a318bbd
henry-maker-commits <hukegong1024@gmail.com>
2023-02-14 10:34:23 +0800

    u

---
+++ contracts/FDao/TeamFundingAllocation.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlEnumerableUpgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract TeamFundingAllocation is Initializable, UUPSUpgradeable, AccessControlEnumerableUpgradeable {
+    
+    address public mainAddr;
+    address[] public secondary;
+    uint256[] public allocationCycle;
+    uint256[] public maximumAmount;
+    uint256[] public billingCycle;
+    uint[] public rate;
+
+    function initialize() public initializer {
+        __UUPSUpgradeable_init();
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+     function _authorizeUpgrade(address newImplementation)
+        internal
+        override
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {}
+    function setMainAddr(address _addr) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        mainAddr = _addr;
+    }
+    
+}
